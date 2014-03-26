778e8609f23d72649f292ddc5965297f36309792
Karthik Chandrasekar <karthidec1@gmail.com>
2014-03-26 04:31:09 -0700

    Code reorg

--- Vandi.py
+++ Vandi.py
@@ -1,31 +1,39 @@
-import sys
+import sys, logging
 
 class RotateTire:
     def __init__(self, numberOfTyres, shiftDirection, skipIndex):
 
+        #Input parameters
         self.numberOfTyres = numberOfTyres
         self.shiftDirection = shiftDirection
-        self.skipIndex = skipIndex
+        self.skipIndex = skipIndex + 1
+
+        #Lists
         self.outputList = []
+        self.tireList = range(self.numberOfTyres)
+        self.checkList = [0] * self.numberOfTyres
+
+        #File
+        self.logging = None
+        self.logger_file = "TireRotation.log"
+
+    def initialize_logger(self):
+        logging.basicConfig(filename=self.logger_file, level=logging.INFO)
+        logging.info("Initialized logger")
+        self.logging = logging
 
     def runMain(self):
         
+        self.initialize_logger()
+        self.logging.info("Number of Tyres - %s, Direction - %s, SkipIndex - %s" % (self.numberOfTyres, self.shiftDirection, self.skipIndex-1))
+         
         #Variables 
+        iterationCount = 0 
         startIndex = 0 
         spare = 'S'
-        self.skipIndex = self.skipIndex + 1
-
-        #Lists
-        self.tireList = range(self.numberOfTyres)
-        self.checkList = [0] * self.numberOfTyres
-        
         print "%s - %s" % (spare, self.tireList)
-        
-        tempList = [spare]
-        tempList.extend(self.tireList)
-        self.outputList.append(tempList)
+        self.addOutput(spare)
 
-        iterationCount = 0 
         fromIndex = (startIndex) % self.numberOfTyres
         toIndex = (startIndex+self.skipIndex) % self.numberOfTyres
         self.rotateTyre(fromIndex, toIndex, spare, iterationCount)
@@ -33,7 +41,6 @@ class RotateTire:
 
     def rotateTyre(self, fromIndex, toIndex, spare, iterationCount):
 
-        #End condition
         while iterationCount < self.numberOfTyres:
 
             #Runs at the beginning of every cycle
@@ -42,7 +49,7 @@ class RotateTire:
                 spare = self.tireList[fromIndex]
                 self.tireList[fromIndex] = temp
                 print "%s - %s" % (spare, self.tireList)
-                self.addOutput(spare, self.tireList)
+                self.addOutput(spare)
             
             #Executes for every element    
             temp = self.tireList[toIndex] 
@@ -50,13 +57,13 @@ class RotateTire:
             spare = temp
             print "%s - %s" % (spare, self.tireList)
             self.checkList[toIndex] = 1
-            self.addOutput(spare, self.tireList)
+            self.addOutput(spare)
      
-            #Clockwise condition
+            #Clockwise direction condition
             if self.shiftDirection == 'C':
                 signDigit = 1
 
-            #Anti-clockwise condition
+            #Anti-clockwise direction condition
             elif self.shiftDirection == 'CC':        
                 signDigit = -1        
  
@@ -68,32 +75,38 @@ class RotateTire:
 
             iterationCount += 1
    
-    def addOutput(self, spare, tyreList):
+    def addOutput(self, spare):
+        #Add state information of tyre rotation
+
         tempList = [spare]
         tempList.extend(self.tireList)
         self.outputList.append(tempList)
         
-
     def displayOutput(self):
-        
+        #Prints the output in the desired form       
+ 
         for index, tyresList in enumerate(self.outputList):
             subList = []
             subList.append(self.formatString(tyresList[0]))
-
             for tyre in tyresList[1:]:
                 subList.append(self.formatString(tyre))
-        
             print "State#%s: %s" % (index,', '.join(subList))
         print 'DONE.'       
         
     def formatString(self, tyre):
+        #Returns the string in desired format
+
         if tyre == 'S':
             tyre = 'Spare'
         else:
             tyre = 'Tire#%s' % (tyre)
         return tyre
- 
 
 if __name__ == "__main__":
-    rotate_obj = RotateTire(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]))
-    rotate_obj.runMain()
+
+    if ( len(sys.argv) == 4):
+        rotate_obj = RotateTire(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]))
+        rotate_obj.runMain()
+
+    else:
+        print "Please enter all the input parameters - Number of tyres, Direction, Skip number"        
